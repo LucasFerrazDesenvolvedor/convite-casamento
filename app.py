dd6dc10c8eae247937f4a8874558bcdedfc700dd
LucasFerrazDesenvolvedo <lucasferreiraferraz@outlook.com>
2026-03-11 20:59:05 -0300

    Atualização e melhorias no convite de casamento

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 import csv
 import os
 from datetime import datetime
@@ -6,11 +6,15 @@ import webbrowser
 
 app = Flask(__name__)
 
+app.secret_key = "segredo_super_casamento"
+
 ARQUIVO = "convidados.csv"
+
+SENHA_ADMIN = "123456"
 
 
 # ==============================
-# CRIAR ARQUIVO CSV SE NÃO EXISTIR
+# CRIAR CSV
 # ==============================
 
 def iniciar_csv():
@@ -18,7 +22,6 @@ def iniciar_csv():
         with open(ARQUIVO, "w", newline="", encoding="utf-8") as f:
             writer = csv.writer(f)
             writer.writerow(["Nome", "Data Confirmacao"])
-
 
 iniciar_csv()
 
@@ -55,14 +58,71 @@ def confirmar():
 
 
 # ==============================
-# INICIAR SERVIDOR
+# LOGIN ADMIN
+# ==============================
+
+@app.route("/admin", methods=["GET","POST"])
+def admin():
+
+    if request.method == "POST":
+
+        senha = request.form.get("senha")
+
+        if senha == SENHA_ADMIN:
+
+            session["logado"] = True
+            return redirect("/lista")
+
+        else:
+            return "Senha incorreta"
+
+    return render_template("login.html")
+
+
+# ==============================
+# LISTA PROTEGIDA
+# ==============================
+
+@app.route("/lista")
+def lista():
+
+    if not session.get("logado"):
+        return redirect("/admin")
+
+    convidados = []
+
+    if os.path.exists(ARQUIVO):
+
+        with open(ARQUIVO, newline="", encoding="utf-8") as f:
+
+            reader = csv.reader(f)
+            next(reader)
+
+            for row in reader:
+                convidados.append(row)
+
+    return render_template("lista.html", convidados=convidados)
+
+
+# ==============================
+# LOGOUT
+# ==============================
+
+@app.route("/logout")
+def logout():
+
+    session.clear()
+    return redirect("/admin")
+
+
+# ==============================
+# SERVIDOR
 # ==============================
 
 if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 5000))
 
-    # abre navegador apenas uma vez
     if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
         webbrowser.open(f"http://127.0.0.1:{port}")
 
